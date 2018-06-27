8debb1603fc6b6190cb07688c76678c6ada4bf31
GROUP\Gillian.Bladen-Clark <gillian.bladen-clark@olmgroup.com>
2018-06-27 17:22:58 +0100

    MyResource - shows how @PathParam and @QueryParam can be applied to member variables

--- advancedrest/src/main/java/org/gillianbc/advancedrest/MyResource.java
+++ advancedrest/src/main/java/org/gillianbc/advancedrest/MyResource.java
@@ -3,20 +3,27 @@ package org.gillianbc.advancedrest;
 import javax.inject.Singleton;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 /**
  * Root resource (exposed at "myresource" path)
  */
-@Path("test")
-@Singleton
+@Path("{something}/test")
+//@Singleton
 /*  By default, a new resource instance is created when it is requested.
  * The @Singleton overrides this so that only one instance of the resource is created
- * (irrespective of no of browser windows)
+ * (irrespective of no of browser windows).  Doesn't work with PathParam
  * */
 public class MyResource {
 	private int count;
+	
+	//e.g. http://localhost:8080/advanced-jaxrs-01/webgbc/apple/test/?somequery=milk
+	@PathParam("something") private String paramfield;
+	@QueryParam("somequery") private String queryfield;
+	
     /**
      * Method handling HTTP GET requests. The returned object will be sent
      * to the client as "text/plain" media type.
@@ -27,6 +34,6 @@ public class MyResource {
     @Produces(MediaType.TEXT_PLAIN)
     public String getIt() {
     	count++;
-        return "Got it!" + count;
+        return "Got it!" + count + " param field was " + paramfield + " query field was " + queryfield;
     }
 }
