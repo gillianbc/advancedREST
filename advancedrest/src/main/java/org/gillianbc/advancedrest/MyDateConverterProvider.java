f674823d9ab3b527697ddb9225abb926dda1bd47
GROUP\Gillian.Bladen-Clark <gillian.bladen-clark@olmgroup.com>
2018-06-27 21:04:38 +0100

    Tut 09 - custom param converter - comments added

--- advancedrest/src/main/java/org/gillianbc/advancedrest/MyDateConverterProvider.java
+++ advancedrest/src/main/java/org/gillianbc/advancedrest/MyDateConverterProvider.java
@@ -7,6 +7,7 @@ import java.util.Calendar;
 import javax.ws.rs.ext.ParamConverter;
 import javax.ws.rs.ext.ParamConverterProvider;
 import javax.ws.rs.ext.Provider;
+
 @Provider
 public class MyDateConverterProvider implements ParamConverterProvider {
 
@@ -14,18 +15,21 @@ public class MyDateConverterProvider implements ParamConverterProvider {
 	public <T> ParamConverter<T> getConverter(final Class<T> rawType, Type genericType, Annotation[] annotations) {
 		if (rawType.getName().equals(MyDate.class.getName())) {
 			return new ParamConverter<T>() {
-
+				// Could have done a new class file, but it's only diddy so the class is defined
+				// inline here
 				@Override
 				public T fromString(String value) {
 					Calendar requestedDate = Calendar.getInstance();
+					// e.g. http://localhost:8080/advanced-jaxrs-01/webgbc/date/tomorrow
 					if ("tomorrow".equalsIgnoreCase(value)) {
 						requestedDate.add(Calendar.DATE, 1);
+						// e.g. http://localhost:8080/advanced-jaxrs-01/webgbc/date/yesterday
 					} else if ("yesterday".equalsIgnoreCase(value)) {
 						requestedDate.add(Calendar.DATE, -1);
 					}
 					// Months are indexed from 0 in Calendar
 					MyDate myDate = new MyDate(requestedDate.get(Calendar.DAY_OF_MONTH),
-							requestedDate.get(Calendar.MONTH)+1, requestedDate.get(Calendar.YEAR));
+							requestedDate.get(Calendar.MONTH) + 1, requestedDate.get(Calendar.YEAR));
 					return rawType.cast(myDate);
 				}
 
