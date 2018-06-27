b0284c78b1409a7ccb57ac418b2d2bd4e0400ece
GROUP\Gillian.Bladen-Clark <gillian.bladen-clark@olmgroup.com>
2018-06-27 16:39:38 +0100

    Tut 5 - using the Application class to expose the resources rather than web.xml

--- advancedrest/src/main/java/org/gillianbc/advancedrest/MyApp.java
+++ advancedrest/src/main/java/org/gillianbc/advancedrest/MyApp.java
@@ -1,8 +1,27 @@
 package org.gillianbc.advancedrest;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
-@ApplicationPath("webapi")
+@ApplicationPath("webgbc")
 public class MyApp extends Application{
-
+/*
+ * By default, the @ApplicationPath annotation above causes jersey to scan through
+ * all the classes on the class path and look for those that have the @Path annotation
+ * and register all of these as resources. 
+ * 
+ *  If you wanted to manually control which resources were available, you could use the 
+ *  method below to make a set of the classes you want available as resources in your app
+ *  
+ *  For example, returning an empty set like this should switch all resources OFF
+ * */
+	
+	public Set<Class<?>> getClasses(){
+		System.out.println("OFF");
+		Set<Class<?>> resourceSet = new HashSet<Class<?>>();
+//		resourceSet.add(MyResource.class);
+		return resourceSet;
+	}
 }
