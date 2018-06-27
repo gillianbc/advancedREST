25ae278aeb827752fca64acb5b9ea8538168c301
GROUP\Gillian.Bladen-Clark <gillian.bladen-clark@olmgroup.com>
2018-06-27 11:22:29 +0100

    Tuts 1-4 done - basic project and web app created

---
+++ advancedrest/src/main/java/org/gillianbc/advancedrest/MyApp.java
@@ -0,0 +1,8 @@
+package org.gillianbc.advancedrest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+@ApplicationPath("webapi")
+public class MyApp extends Application{
+
+}
